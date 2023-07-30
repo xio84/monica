986dfd525cf5613c4553f5f599669fbfebafe29a
Oksidian Tafly <oksitafly@gmail.com>
2023-07-30 12:10:50 +0800

    + Migrations: Fix null columns

---
+++ database/migrations/2023_07_30_030516_fix_nullable_columns.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('contacts', function (Blueprint $table) {
+            $table->string('default_avatar_color')->nullable()->change();
+        });
+        Schema::table('journal_entries', function (Blueprint $table) {
+            $table->string('journalable_type')->nullable()->change();
+            $table->integer('journalable_id')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
